a07b9f41963d90b1807374db7fccb9e16f8be28d
karllundgren <39705381+karllundgren@users.noreply.github.com>
2022-07-19 14:47:45 -0500

    Update hardcodedSecret.py

--- hardcodedSecret.py
+++ hardcodedSecret.py
@@ -1,4 +1,4 @@
-AWSCredentials = [["AKIAJSIE27KKMHXI3BJQ", "5bEYu26084qjSFyclM/f2pz4gviSfoOg+mFwBH39"],["AKIAIOSFODNN7EXAMPLE" "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"]]
-APIKeyCredentials = ["sk_0b9b5db6-fabc-49d0-b68f-12343dd4f808"]
-print(str(AWSCredentials))
-print(APIKeyCredentials)
+FakeAWSCredentials = [["AKIAJSIE27KKMHXI3BJQ", "5bEYu26084qjSFyclM/f2pz4gviSfoOg+mFwBH39"],["AKIAIOSFODNN7EXAMPLE" "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"]]
+FakeAPIKeyCredentials = ["sk_0b9b5db6-fabc-49d0-b68f-12343dd4f808"]
+print(str(FakeAWSCredentials))
+print(FakeAPIKeyCredentials)
